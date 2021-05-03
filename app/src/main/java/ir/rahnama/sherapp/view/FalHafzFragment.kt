782fd91b08ee44ec5e73d722e44534512b952685
Ali Nabavi <arz1379n@gmail.com>
2021-05-03 14:26:16 +0430

    اضافه شدن فال حافط و logo

--- app/src/main/java/ir/rahnama/sherapp/view/FalHafzFragment.kt
+++ app/src/main/java/ir/rahnama/sherapp/view/FalHafzFragment.kt
@@ -4,37 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import ir.rahnama.sherapp.R
+import androidx.lifecycle.Observer
+import dagger.hilt.android.AndroidEntryPoint
 import ir.rahnama.sherapp.databinding.FargmentFalHafzBinding
-import ir.rahnama.sherapp.databinding.FragmentShowPoemsBinding
-import ir.rahnama.sherapp.model.SelectionPoetryModel
 import ir.rahnama.sherapp.utiles.*
-import ir.rahnama.sherapp.view.adapter.BookContentAdapter
-import ir.rahnama.sherapp.view.adapter.PoemBodyAdapter
-import ir.rahnama.sherapp.view.adapter.PoemBodySpaceItem
+import ir.rahnama.sherapp.utiles.Resource.*
+import ir.rahnama.sherapp.utiles.Resource.Status.*
+import ir.rahnama.sherapp.view.adapter.*
 import ir.rahnama.sherapp.viewmodel.BookContentViewModel
 import ir.rahnama.sherapp.viewmodel.BooksViewModel
 import ir.rahnama.sherapp.viewmodel.PoemBodyViewModel
+import ir.rahnama.sherapp.viewmodel.PoetryViewModel
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.fargment_fal_hafz.*
-import java.util.Observer
+import kotlinx.android.synthetic.main.fragment_books_list.*
+import java.util.*
+import kotlin.properties.Delegates
 
+@AndroidEntryPoint
 class FalHafzFragment : Fragment(){
 
-    private var poetryList: MutableList<SelectionPoetryModel> = arrayListOf()
-
-    private val viewModel: BookContentViewModel by viewModels()
     private val poemViewModel : PoemBodyViewModel by viewModels()
     private val poemBodyAdapter = PoemBodyAdapter()
     private var binding: FargmentFalHafzBinding by autoCleared()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FargmentFalHafzBinding.inflate(inflater, container, false)
         binding.poemBodyRecyclerViewHafez.adapter = poemBodyAdapter
         binding.poemBodyRecyclerViewHafez.addItemDecoration(PoemBodySpaceItem())
@@ -44,25 +46,76 @@ class FalHafzFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val id = "24"
+//        val name = "حافظ"
+//        poetryViewModel.getPoetryNames(name)
+//        poetryViewModel.poetryNames.observe(viewLifecycleOwner , Observer {
+//            when (it.status) {
+//                SUCCESS -> it.data?.let { categoryAdapter.refreshData(it) }
+//                ERROR -> { it.message?.let { requireActivity().toast(it) } }
+//                LOADING -> {}
+//            }
+//        })
+//
+//
+//        val bookId = "24"
+//        bookViewModel.getBooksList(bookId)
+//        bookViewModel.books.observe(viewLifecycleOwner , Observer {
+//            when (it.status) {
+//                SUCCESS -> it.data?.let { bookAdapter.refreshData(it) }
+//                ERROR -> it.message?.let { requireActivity().toast(it) }
+//                LOADING ->{}
+//            }
+//        })
+//
+//
+//        val contentId = bookAdapter.itemCount
+//        bookContentViewModel.getBookContent(contentId.toString())
+//        bookContentViewModel.bookContent.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+//            when (it.status) {
+//                SUCCESS -> it.data?.let { bookContentAdapter.refreshData(it) }
+//                ERROR ->  it.message?.let { requireActivity().toast(it) }
+//                LOADING -> {}
+//            }
+//        })
+//
+//        val size = bookContentAdapter.itemCount
+//        poemViewModel.getPoemById(size.toString())
 
+        // from 2093 to 2624
+        val id = Random().nextInt(573) + 2051
+        poemViewModel.getPoemById(id.toString())
+        observeModel()
 
-        fal_hafez.setOnClickListener{
-
-
+        fal_hafez.setOnClickListener {
 
             fal_hafez.setInvisible()
-            poemBodyRecyclerViewHafez.setVisible()
+            fal_hafez_background.setVisible()
+
         }
-
+    }
+    private fun observeModel(){
+        poemViewModel.poemBody.observe(viewLifecycleOwner , androidx.lifecycle.Observer {
+            when (it.status) {
+                SUCCESS -> it.data?.let {
+                    poemBodyAdapter.updatePoems(it)
+                }
+                ERROR -> {
+                    it.message?.let { requireActivity().toast(it) }
+                }
+                LOADING -> {
+                }
+            }
+        })
+    }
     }
 
-    fun getPoem(){
 
 
 
 
-    }
 
 
-}
+
+
+
+
